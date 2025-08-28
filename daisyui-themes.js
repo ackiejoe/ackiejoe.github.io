dc40970a63a598b237bc4400ea0d457396b77969
Alex <agmorrison@gmail.com>
2025-08-27 21:58:31 -0300

    adding daisyUI theme

---
+++ daisyui-themes.js
@@ -0,0 +1,4 @@
+// DaisyUI theme list for use in theme selection
+window.DaisyUIThemes = [
+  "light", "dark", "cupcake", "bumblebee", "emerald", "corporate", "synthwave", "retro", "cyberpunk", "valentine", "halloween", "garden", "forest", "aqua", "lofi", "pastel", "fantasy", "wireframe", "black", "luxury", "dracula", "cmyk", "autumn", "business", "acid", "lemon", "night", "coffee", "winter"
+];
